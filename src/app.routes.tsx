807b5e736437b2f5590b5676e96f3461f76c17c4
Wesley Moraes"git config --global user.name Wesley <wesleyjava88@gmail.com>
2023-03-14 22:57:51 -0300

    feat: adding route of the game page

--- src/app.routes.tsx
+++ src/app.routes.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Home } from "./components/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Game } from "./page/Game";
 
 export const AppRoute: React.FC = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
