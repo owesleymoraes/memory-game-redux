949d2f2a13ab3ca266ca98fc87ed0c4c7b426dba
Wesley Moraes"git config --global user.name Wesley <wesleyjava88@gmail.com>
2023-03-19 17:15:24 -0300

    feat: adding a hash in state of cards

--- src/redux/sliceSpinCard.ts
+++ src/redux/sliceSpinCard.ts
@@ -1,10 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { AppDispatch, AppThunk } from "./store";
 
 interface CardProps {
   id: number;
   key: number;
   nameCard: string;
   spin: boolean;
+  hasCardMatch: boolean;
 }
 
 interface SpinCard {
@@ -15,9 +17,12 @@ interface SpinCard {
 const INITIAL_STATE: SpinCard = {
   spinCard: true,
   listCards: [
-    { id: 1, key: 1, nameCard: "carta 01", spin: true },
-    { id: 2, key: 2, nameCard: "carta 02", spin: true },
-    { id: 3, key: 3, nameCard: "carta 03", spin: true },
+    { id: 1, key: 1, nameCard: "carta 01", spin: true, hasCardMatch: false },
+    { id: 2, key: 2, nameCard: "carta 02", spin: true, hasCardMatch: false },
+    { id: 3, key: 3, nameCard: "carta 03", spin: true, hasCardMatch: false },
+    { id: 1, key: 4, nameCard: "carta 01", spin: true, hasCardMatch: false },
+    { id: 2, key: 5, nameCard: "carta 02", spin: true, hasCardMatch: false },
+    { id: 3, key: 6, nameCard: "carta 03", spin: true, hasCardMatch: false },
   ],
 };
 
@@ -29,10 +34,24 @@ const sliceSpinCard = createSlice({
     changeSpinCard: (state, action) => {
       const cards = state.listCards.slice();
       const index = cards.findIndex((c) => c.key === action.payload);
+      const otherIndex = cards.findIndex((c) => !c.spin && !c.hasCardMatch);
+
       if (index > -1) {
-        cards[index].spin = !cards[index].spin;
+        if(!cards[index].spin){
+          return state
+        }
+        if (otherIndex > -1) {
+          if (cards[index].id === cards[otherIndex].id) {
+            cards[index].spin = !cards[index].spin;
+            cards[index].hasCardMatch = true;
+            cards[otherIndex].hasCardMatch = true;
+          } else {
+            cards[otherIndex].spin = !cards[otherIndex].spin;
+          }
+        } else {
+          cards[index].spin = !cards[index].spin;
+        }
       }
-      
     },
   },
 });
@@ -44,3 +63,7 @@ export const { changeSpinCard } = sliceSpinCard.actions;
 export const useSpinCard = (state: any) => {
   return state.spinCardStore as SpinCard;
 };
+
+export function asyncShowCard(): AppThunk {
+  return async function (dispatch) {};
+}
